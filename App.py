c55539211eed7eff928067e14d2746ce03d1c40f
MauroCD5 <61813030+MauroCD5@users.noreply.github.com>
2021-01-01 19:29:11 -0600

    crud productos

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_DB']='worldculture'
 mysql = MySQL(app)
+
+#configuracion
+app.secret_key = 'mykey'
 
 #Rutas y funciones
 @app.route('/')
@@ -20,7 +23,10 @@ def Productos():
 
 @app.route('/AdministrarProductos')
 def AdministrarProductos():
-    return render_template('AdministrarProductos.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM productos')
+    data = cur.fetchall()
+    return render_template('AdministrarProductos.html', productos = data)
 
 @app.route('/AddProductos', methods=['POST'])
 def add_productos():
@@ -37,11 +43,52 @@ def add_productos():
         cur.execute('INSERT INTO productos (Codigo, Nombre, Descripcion, Departamento, Seccion, Stock, PrecioVenta, PrecioCompra) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
         (Codigo, Nombre, Descripcion, Departamento, Seccion, Stock, PrecioVenta, PrecioCompra))
         mysql.connection.commit()
-
-        return 'El producto fue guardado correctamente'
+        flash('El producto se agrego de manera correcta')
+        return redirect(url_for('AdministrarProductos'))
     
+@app.route('/EditProductos/<Id>')
+def EditProductos(Id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM productos WHERE Id = %s', (Id))  
+    data = cur.fetchall()
+    return render_template('EditProductos.html', producto = data[0])
 
+@app.route('/update/<Id>', methods=['POST'])
+def update_contact(Id):
+    if request.method == 'POST':
+        Codigo = request.form['Codigo']
+        Nombre = request.form['Nombre']
+        Descripcion = request.form['Descripcion']
+        Departamento = request.form['Departamento']
+        Seccion = request.form ['Seccion']
+        Stock = request.form['Stock']
+        precioVenta = request.form['PrecioVenta']
+        precioCompra = request.form['PrecioCompra']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE productos
+            SET 
+                Codigo = %s,
+                Nombre= %s,
+                Descripcion = %s,
+                Departamento = %s,
+                Seccion = %s,
+                Stock = %s,
+                PrecioVenta = %s,
+                PrecioCompra = %s
+            WHERE Id = %s
+        """, (Codigo, Nombre, Descripcion, Departamento, Seccion, Stock, precioVenta, precioCompra, Id))
+        flash('Producto editado de manera correcta')
+        mysql.connection.commit()
+        return redirect(url_for('AdministrarProductos'))
 
+@app.route('/DeleteProductos/<string:Id>')
+def DeleteProductos(Id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM productos WHERE Id = {0}'.format(Id))
+    mysql.connection.commit()
+    flash('Producto eliminado de manera correcta')
+    return redirect(url_for('AdministrarProductos'))
 
 if __name__== '__main__':
     app.run(port = 3000, debug = True)
